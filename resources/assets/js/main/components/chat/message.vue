1ef635198b091005e664e645cb82de285d4bce45
wang <wang@126.com>
2020-10-30 14:39:35 +0800

    update

---
+++ resources/assets/js/main/components/chat/message.vue
@@ -0,0 +1,310 @@
+<template>
+    <div :data-id="info.id">
+
+        <!--文本、任务、报告、视频通话-->
+        <div v-if="info.type==='text' || info.type==='taskB' || info.type==='report' || info.type==='video' || info.type==='voice'">
+            <div v-if="info.self===true" class="list-right">
+                <div v-if="info.error" class="item-error" @click="clickError(info.error)">
+                    <Icon type="md-alert" />
+                </div>
+                <div class="item-right">
+                    <div class="item-username" @click="clickUser">
+                        <em class="item-name"><user-view :username="info.username" placement="left"/></em>
+                        <em v-if="info.indate" class="item-date">{{formatCDate(info.indate)}}</em>
+                    </div>
+                    <div class="item-text" :class="{'text-error':info.error}">
+                        <div class="item-text-view">{{textMsg(info.text)}}</div>
+                    </div>
+                    <template v-if="info.type==='taskB'">
+                        <div v-if="info.other.type==='task'" class="item-link" @click="taskDetail(info.other.id)"><span>{{$L('来自关注任务')}}:</span><a href="javascript:void(0)">{{info.other.title}}</a></div>
+                        <div v-if="info.other.type==='file'" class="item-link"><span>{{$L('来自关注任务')}}:</span><a target="_blank" :href="fileDownUrl(info.other.id)">{{info.other.name}}</a></div>
+                    </template>
+                    <div v-else-if="info.type==='report'" class="item-link" @click="reportDetail(info.other.id, info.other.title)"><span>{{$L('来自工作报告')}}:</span><a href="javascript:void(0)">{{info.other.title}}</a></div>
+                    <div v-else-if="info.type==='video' || info.type==='voice'" class="item-link">
+                        <Icon v-if="info.type==='voice'" type="ios-call-outline"/>
+                        <Icon v-else type="ios-videocam-outline"/>
+                        <span>{{$L('通话时长：%', formatSecond(info.other.second))}}</span>
+                    </div>
+                </div>
+                <img class="item-userimg" @click="clickUser" :src="info.userimg" onerror="this.src=window.location.origin+'/images/other/avatar.png'"/>
+            </div>
+            <div v-else-if="info.self===false" class="list-item">
+                <img class="item-userimg" @click="clickUser" :src="info.userimg" onerror="this.src=window.location.origin+'/images/other/avatar.png'"/>
+                <div class="item-left">
+                    <div class="item-username" @click="clickUser">
+                        <em class="item-name"><user-view :username="info.username" placement="right"/></em>
+                        <em v-if="info.__usertag" class="item-tag">{{info.__usertag}}</em>
+                        <em v-if="info.indate" class="item-date">{{formatCDate(info.indate)}}</em>
+                    </div>
+                    <div class="item-text" :class="{'text-error':info.error}">
+                        <div class="item-text-view">{{textMsg(info.text)}}</div>
+                    </div>
+                    <template v-if="info.type==='taskB'">
+                        <div v-if="info.other.type==='task'" class="item-link" @click="taskDetail(info.other.id)"><span>{{$L('来自关注任务')}}:</span><a href="javascript:void(0)">{{info.other.title}}</a></div>
+                        <div v-if="info.other.type==='file'" class="item-link"><span>{{$L('来自关注任务')}}:</span><a target="_blank" :href="fileDownUrl(info.other.id)">{{info.other.name}}</a></div>
+                    </template>
+                    <div v-else-if="info.type==='report'" class="item-link" @click="reportDetail(info.other.id, info.other.title)"><span>{{$L('来自工作报告')}}:</span><a href="javascript:void(0)">{{info.other.title}}</a></div>
+                    <div v-else-if="info.type==='video' || info.type==='voice'" class="item-link">
+                        <Icon v-if="info.type==='voice'" type="ios-call-outline"/>
+                        <Icon v-else type="ios-videocam-outline"/>
+                        <span>{{$L('通话时长：%', formatSecond(info.other.second))}}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!--图片-->
+        <div v-else-if="info.type==='image'">
+            <div v-if="info.self===true" class="list-right">
+                <div v-if="info.error" class="item-error" @click="clickError(info.error)">
+                    <Icon type="md-alert" />
+                </div>
+                <div class="item-right">
+                    <div class="item-username" @click="clickUser">
+                        <em class="item-name"><user-view :username="info.username" placement="left"/></em>
+                        <em v-if="info.indate" class="item-date">{{formatCDate(info.indate)}}</em>
+                    </div>
+                    <a class="item-image" :href="info.url" target="_blank">
+                        <img class="item-image-view" :src="info.url"/>
+                    </a>
+                </div>
+                <img class="item-userimg" @click="clickUser" :src="info.userimg" onerror="this.src=window.location.origin+'/images/other/avatar.png'"/>
+            </div>
+            <div v-else-if="info.self===false" class="list-item">
+                <img class="item-userimg" @click="clickUser" :src="info.userimg" onerror="this.src=window.location.origin+'/images/other/avatar.png'"/>
+                <div class="item-left">
+                    <div class="item-username" @click="clickUser">
+                        <em class="item-name"><user-view :username="info.username" placement="right"/></em>
+                        <em v-if="info.__usertag" class="item-tag">{{info.__usertag}}</em>
+                        <em v-if="info.indate" class="item-date">{{formatCDate(info.indate)}}</em>
+                    </div>
+                    <a class="item-image" :href="info.url" target="_blank">
+                        <img class="item-image-view" :src="info.url"/>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <!--通知-->
+        <div v-else-if="info.type==='notice'">
+            <div class="item-notice">{{info.notice}}</div>
+        </div>
+
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    /*通用*/
+    .list-item, .list-right {
+        display: flex;
+        width: 100%;
+        padding-top: 7px;
+        padding-bottom: 7px;
+        background-color: #E8EBF2;
+        .item-left, .item-right {
+            display: flex;
+            flex-direction: column;
+            max-width: 80%;
+            .item-username {
+                font-size: 12px;
+                padding-top: 1px;
+                padding-bottom: 4px;
+                display: flex;
+                flex-direction: row;
+                align-items: center;
+                em {
+                    display: inline-block;
+                    font-style: normal;
+                    &.item-name {
+                        color: #888888;
+                    }
+                    &.item-tag {
+                        color: #ffffff;
+                        background-color: #ff0000;
+                        line-height: 16px;
+                        padding: 2px 4px;
+                        margin-left: 3px;
+                        border-radius: 2px;
+                        font-size: 12px;
+                        transform: scale(0.8);
+                        font-weight: 600;
+                    }
+                    &.item-date {
+                        margin-left: 4px;
+                        color: #aaaaaa;
+                    }
+                }
+            }
+        }
+        .item-left {
+            align-items: flex-start;
+        }
+        .item-right {
+            align-items: flex-end;
+            .item-username {
+                text-align: right;
+            }
+            .item-link {
+                transform-origin: right center;
+            }
+        }
+        .item-userimg {
+            width: 38px;
+            height: 38px;
+            margin-left: 8px;
+            margin-right: 8px;
+            border-radius: 3px;
+        }
+        .item-error {
+            cursor: pointer;
+            width: 48px;
+            position: relative;
+            > i {
+                color: #ff0000;
+                font-size: 18px;
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+            }
+        }
+    }
+
+    .list-right {
+        justify-content: flex-end;
+    }
+
+    /*文本*/
+    .item-text {
+        display: inline-block;
+        border-radius: 6px;
+        padding: 8px;
+        background-color: #ffffff;
+        max-height: 580px;
+        overflow: auto;
+        &.text-error {
+            box-shadow: 0 0 4px 0 #ffa1a1;
+        }
+        .item-text-view {
+            max-width: 520px;
+            color: #242424;
+            font-size: 14px;
+            line-height: 18px;
+            word-break: break-all;
+        }
+    }
+
+    /*信息底标*/
+    .item-link {
+        display: flex;
+        align-items: center;
+        max-width: 100%;
+        font-size: 12px;
+        color: #ffffff;
+        background-color: #cacaca;
+        margin-top: 6px;
+        margin-bottom: -2px;
+        height: 20px;
+        line-height: 20px;
+        padding: 0 5px;
+        border-radius: 4px;
+        transform: scale(0.96);
+        transform-origin: left center;
+        > i {
+            font-size: 14px;
+            padding-right: 2px;
+        }
+        > span {
+            white-space: nowrap;
+        }
+        > a {
+            color: #3D90E2;
+            padding-left: 3px;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+            max-width: 100%;
+        }
+    }
+
+    /*图片*/
+    .item-image {
+        display: inline-block;
+        text-decoration: none;
+        .item-image-view {
+            max-width: 220px;
+            max-height: 220px;
+            border-radius: 6px;
+        }
+    }
+
+    /*通知*/
+    .item-notice {
+        color: #777777;
+        font-size: 12px;
+        text-align: center;
+        padding: 12px 24px;
+    }
+</style>
+
+<script>
+
+    export default {
+        name: 'ChatMessage',
+        props: {
+            info: {
+                type: Object,
+                default: {},
+            },
+        },
+
+        mounted() {
+
+        },
+
+        methods: {
+            textMsg(text) {
+                return (text + "").replace(/\n/, '<br/>');
+            },
+
+            formatCDate(v) {
+                let string = '';
+                if ($A.runNum(v) > 0) {
+                    if ($A.formatDate('Ymd') === $A.formatDate('Ymd', v)) {
+                        string = $A.formatDate('H:i', v)
+                    } else if ($A.formatDate('Y') === $A.formatDate('Y', v)) {
+                        string = $A.formatDate('m-d', v)
+                    } else {
+                        string = $A.formatDate('Y-m-d', v)
+                    }
+                }
+                return string ? '(' + string + ')' : '';
+            },
+
+            clickError(err) {
+                this.$Modal.error({
+                    title: this.$L("错误详情"),
+                    content: err
+                });
+            },
+
+            clickUser(e) {
+                this.$emit('clickUser', this.info, e);
+            },
+
+            fileDownUrl(id) {
+                return $A.apiUrl('project/files/download?fileid=' + id);
+            },
+
+            formatSecond(d) {
+                if (d > 3600) {
+                    return Math.ceil(d / 3600) + '小时';
+                } else if (d > 60) {
+                    return Math.ceil(d / 60) + '分钟';
+                } else {
+                    return d + '秒';
+                }
+            }
+        }
+    }
+</script>
---
+++ resources/assets/js/main/pages/login.vue
@@ -0,0 +1,241 @@
+<template>
+    <div class="login_body">
+        <div class="modal_box">
+            <div class="modal_l">
+                <img src="../../../statics/images/login_mark_img.png" alt="">
+            </div>
+            <div class="modal_r">
+                <div class="login_title">HI~</div>
+                <div class="login_title2">欢迎登录云迈团队协作平台</div>
+                <div class="login-header">
+                    <div @click="loginType='login'" class="login-header-item" :class="{active:loginType=='login'}">{{$L('用户登录')}}</div>
+                    <div v-if="systemConfig.reg=='open'" @click="loginType='reg'" class="login-header-item" :class="{active:loginType=='reg'}">{{$L('注册账号')}}</div>
+                </div>
+                <Form ref="login" :model="formLogin" :rules="ruleLogin">
+                    <FormItem prop="username">
+                        <Input type="text" v-model="formLogin.username" size="large" :placeholder="$L('用户名')" @on-enter="onLogin">
+                            <Icon type="ios-person-outline" slot="prepend"></Icon>
+                        </Input>
+                    </FormItem>
+                    <FormItem prop="userpass">
+                        <Input type="password" v-model="formLogin.userpass" size="large" :placeholder="$L('密码')" @on-enter="onLogin">
+                            <Icon type="ios-lock-outline" slot="prepend"></Icon>
+                        </Input>
+                    </FormItem>
+                    <FormItem v-if="loginType=='reg'" prop="userpass2">
+                        <Input type="password" v-model="formLogin.userpass2" size="large" :placeholder="$L('确认密码')" @on-enter="onLogin">
+                            <Icon type="ios-lock-outline" slot="prepend"></Icon>
+                        </Input>
+                    </FormItem>
+                </Form>
+                <div class="modal_btn">
+                    <Button type="default" size="large" @click="loginShow=false">{{$L('取消')}}</Button>
+                    <Button type="primary" size="large" :loading="loadIng > 0" @click="onLogin">{{$L(loginType=='reg'?'注册':'登录')}}</Button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+    .login_body{
+        display:flex;
+        align-items:center;
+        justify-content:center;
+        background: url('../../../statics/images/login_bg.jpg') no-repeat center;background-size: cover;
+    }
+    .modal_box{
+        display: flex;
+        align-items: flex-start;
+        justify-content: space-between;
+        position: relative;
+        max-width: 1200px;width: 80%;height: 65vh;
+        max-height: 600px;padding: 10px;
+        border-radius: 10px;box-shadow: 0px 0px 10px rgba(0,0,0,.1);
+        background:#fff url('../../../statics/images/login_mark_bg.png') no-repeat left bottom;
+        background-size:auto 80%;
+    }
+    .modal_l{
+        display: flex;
+        width: 58%;
+        height: 100%;
+        justify-content: center;
+        align-items: center;
+    }
+    .modal_l img{
+        display: block;
+        width: 90%;
+    }
+    .modal_r{
+        display: block;
+        width: 35%;
+        margin-right: 5%;
+    }
+    .modal_btn{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+    .modal_btn button{
+        width: 49%;
+    }
+.login_title{font-size: 40px;color: #007aff;font-weight: 600;line-height: 1.4;margin-top: 40px;}
+.login_title2{font-size: 30px;color: #007aff;line-height: 1.4;}
+    .login-header {
+        display: flex;
+        align-items: center;
+        margin-top: 20px;
+        margin-bottom: 20px;
+        .login-header-item {
+            height: 40px;
+            line-height: 40px;
+            font-size: 20px;
+            color: #444444;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+            padding-right: 12px;
+            cursor: pointer;
+            &.active {
+                font-size: 25px;
+                color: #2d8cf0;
+                font-weight: 500;
+            }
+        }
+    }
+</style>
+<script>
+    export default {
+        data () {
+            return {
+                loadIng: 0,
+                loginShow: true,
+                loginType: 'login',
+
+                formLogin: {
+                    username: '',
+                    userpass: '',
+                    userpass2: ''
+                },
+                ruleLogin: {},
+
+                systemConfig: $A.jsonParse($A.storage("systemSetting"), {
+                    logo: '',
+                    github: '',
+                    reg: '',
+                }),
+
+                fromUrl: '',
+            }
+        },
+        created() {
+            // this.ruleLogin = {
+            //     username: [
+            //         { required: true, message: this.$L('请填写用户名！'), trigger: 'change' },
+            //         { type: 'string', min: 2, message: this.$L('用户名长度至少2位！'), trigger: 'change' }
+            //     ],
+            //     userpass: [
+            //         { required: true, message: this.$L('请填写登录密码！'), trigger: 'change' },
+            //         { type: 'string', min: 6, message: this.$L('密码错长度至少6位！'), trigger: 'change' }
+            //     ],
+            //     userpass2: [
+            //         { required: true, message: this.$L('请填写确认密码！'), trigger: 'change' },
+            //         { type: 'string', min: 6, message: this.$L('确认密码错长度至少6位！'), trigger: 'change' },
+            //         {
+            //             validator: (rule, value, callback) => {
+            //                 if (value !== this.formLogin.userpass) {
+            //                     callback(new Error(this.$L('两次密码输入不一致！')));
+            //                 } else {
+            //                     callback();
+            //                 }
+            //             },
+            //             required: true,
+            //             trigger: 'change'
+            //         },
+            //     ]
+            // };
+        },
+        mounted() {
+            // this.getSetting();
+            // this.fromUrl = decodeURIComponent($A.getObject(this.$route.query, 'from'));
+            // if (this.fromUrl) {
+            //     this.loginChack();
+            // }
+        },
+        deactivated() {
+            this.loginShow = false;
+        },
+        watch: {
+            loginShow(val) {
+                if (val) {
+                    this.getSetting();
+                } else {
+                    this.loginType = 'login';
+                }
+            }
+        },
+        methods: {
+            getSetting() {
+                $A.apiAjax({
+                    url: 'system/setting',
+                    error: () => {
+                        $A.storage("systemSetting", {});
+                    },
+                    success: (res) => {
+                        if (res.ret === 1) {
+                            this.systemConfig = res.data;
+                            this.systemConfig.github = this.systemConfig.github || 'show';
+                            this.systemConfig.reg = this.systemConfig.reg || 'open';
+                            $A.storage("systemSetting", this.systemConfig);
+                        } else {
+                            $A.storage("systemSetting", {});
+                        }
+                    }
+                });
+            },
+
+            loginChack() {
+                if ($A.getToken() !== false) {
+                    this.goForward({path: '/todo'}, true);
+                } else {
+                    this.loginShow = true;
+                }
+            },
+            onLogin() {
+                this.$refs.login.validate((valid) => {
+                    if (valid) {
+                        this.loadIng++;
+                        $A.ajax({
+                            url: $A.apiUrl('users/login?type=' + this.loginType),
+                            data: this.formLogin,
+                            complete: () => {
+                                this.loadIng--;
+                            },
+                            success: (res) => {
+                                if (res.ret === 1) {
+                                    $A.storage("userInfo", res.data);
+                                    $A.setToken(res.data.token);
+                                    $A.triggerUserInfoListener(res.data);
+                                    //
+                                    this.loadIng--;
+                                    this.loginShow = false;
+                                    this.$refs.login.resetFields();
+                                    this.$Message.success(this.$L('登录成功'));
+                                    if (this.fromUrl) {
+                                        window.location.replace(this.fromUrl);
+                                    } else {
+                                        this.goForward({path: '/todo'}, true);
+                                    }
+                                } else {
+                                    this.$Modal.error({
+                                        title: this.$L("温馨提示"),
+                                        content: res.msg
+                                    });
+                                }
+                            }
+                        })
+                    }
+                })
+            }
+        },
+    }
+</script>
